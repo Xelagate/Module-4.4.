3511648805917878f61b821695451ad8520f5ebd
Anton Makhankov <36548646+vvayffaring@users.noreply.github.com>
2022-01-01 13:37:59 +0300

    Delete dist directory

--- dist/mappings/mappingHandlers.d.ts
+++
@@ -1,3 +0,0 @@
-import { SubstrateEvent } from "@subql/types";
-export declare function handleStakingReward(event: SubstrateEvent): Promise<void>;
-export declare function handleSumReward(event: SubstrateEvent): Promise<void>;
